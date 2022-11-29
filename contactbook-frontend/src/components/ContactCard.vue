e2a24103c29d0a4d16725f3dc63a2eee6a21dd58
anhbaonguyenle <anhlenguyenbaoanh@gmail.com>
2022-11-29 22:25:18 +0700

    Setup Frontend

---
+++ contactbook-frontend/src/components/ContactCard.vue
@@ -0,0 +1,33 @@
+<script>
+export default {
+    props: {
+        contact: { type: Object, required: true },
+    },
+};
+</script>
+<template>
+    <div>
+        <div class="p-1">
+            <strong>Tên:</strong>
+            {{ contact.name }}
+
+        </div>
+        <div class="p-1">
+            <strong>E-mail:</strong>
+            {{ contact.email }}
+        </div>
+        <div class="p-1">
+            <strong>Địa Chỉ:</strong>
+            {{ contact.address }}
+        </div>
+        <div class="p-1">
+            <strong>Điện Thoại:</strong>
+            {{ contact.phone }}
+        </div>
+        <div class="p-1">
+            <strong>Favorite:&nbsp;</strong>
+            <i v-if="contact.favorite" class="fas fa-check" />
+            <i v-else class="fas fa-times" />
+        </div>
+    </div>
+</template>
